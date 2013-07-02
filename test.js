237c2ed8219528891b73bf0fad2c074362f57dd1
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-07-01 23:32:34 -0400

    adding in other .chicken files from torso.me

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var read = require("fs").readFileSync;
 var chicken = require("./chicken");
 
 console.assert(chicken("","chicken") === "chicken");
-console.assert(chicken("test foo", read("echo.chicken") + "") == "test foo")
+console.assert(chicken("test foo", read("echocat.chicken") + "") == "test foo")
 console.assert(chicken("", read("hello.chicken") + "") == "&#72;&#101;&#108;&#108;&#111;&#32;&#119;&#111;&#114;&#108;&#100;");
 
 console.log(chicken(0, read("99.chicken")+""));
