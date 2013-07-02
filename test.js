8fbae36a0b980c6f2d5bff59a503dc84d5f083fb
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-07-02 09:00:02 -0400

    fixing test.js by uncommiting some tests and adding in new file paths

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var read = require("fs").readFileSync;
 
 var chicken = require("./chicken");
 
-//console.assert(chicken("","chicken") === "chicken");
-//console.assert(chicken("test foo", read("echocat.chicken") + "") == "test foo")
- console.assert(chicken("", read("hello.chicken") + "") == "&#72;&#101;&#108;&#108;&#111;&#32;&#119;&#111;&#114;&#108;&#100;");
-// console.assert(chicken(0, read("99.chicken")+"") == "n&#111;&#32;chicken&#115;&#10;");
+console.assert(chicken("","chicken") === "chicken");
+console.assert(chicken("test foo", read("chicken/echocat.chicken") + "") == "test foo")
+console.assert(chicken("", read("chicken/hello.chicken") + "") == "&#72;&#101;&#108;&#108;&#111;&#32;&#119;&#111;&#114;&#108;&#100;");
+console.assert(chicken(0, read("chicken/99.chicken")+"") == "n&#111;&#32;chicken&#115;&#10;");
